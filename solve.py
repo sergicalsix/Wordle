14f6a8128fc5fdef9d868940e98f7fb8c9ba386a
sergicalsix <kenp3434membertip1@gmail.com>
2022-02-21 23:20:53 +0900

    solve one x

--- solve.py
+++ solve.py
@@ -1,45 +1,58 @@
 import pandas as pd
-import copy
 #https://github.com/alex1770/wordle/blob/main/wordlist_hidden
+
 df = pd.read_csv('wordle_ans.csv',header=None)
+#df = pd.read_csv('wordle_all.csv',header=None)
 #print(df.head())
 ans0:list = df[0].values
 
-pos_dict = {3:'i'}
-character_list = ['i','s']
-non_character_list = ['r','e','a','t','o','u','g','h','p','k','y']
+pos_dict = {4:'e',5:'r'}
+character_list = ['t','r','o','e']
+non_character_list = ['a','s','i','f','l','u','w']
+#non_character_list = []
 
 def character_select(ans_candidate,character_list:list):
+    if len(character_list) == 0:
+        return ans_candidate
     ans_set = set([])
     for c in character_list:
+        ans_ = [a for a in ans_candidate if c in a]
         if len(ans_set) == 0:
-            ans_set = set([a for a in ans_candidate if c in a]) 
+            ans_set = set(ans_)
         else:
-            ans_set = ans_set.intersection(set([a for a in ans_candidate if c in a]) )
+            ans_set = ans_set.intersection(set(ans_))
     return list(ans_set)
 
-def character_position_select(ans_candidate,character_postion_dict):
+def character_not_select(ans_candidate,character_list:list):
+    if len(character_list) == 0:
+        return ans_candidate
+    ans_set = set([])
+    for c in character_list:
+        ans_ = [a for a in ans_candidate if c not in a]
+        if len(ans_set) == 0:
+            ans_set = set(ans_)
+        else:
+            ans_set = ans_set.intersection(set(ans_))
+    return list(ans_set)
+
+def character_position_select(ans_candidate,character_position_dict):
+    if len(character_position_dict.keys()) == 0:
+        return ans_candidate
     """
     character_postion_dict: {1:'f'}
     """
-    ans_ = copy.deepcopy(ans_candidate)
-
-    for k,v in character_postion_dict.items():
-        ans_ = [a for a in ans_ if a[int(k) - 1] == v]
-    return ans_
-
-def character_not_select(ans_candidate,character_list:list):
     ans_set = set([])
-    for c in character_list:
+    for k,v in character_position_dict.items():
+        ans_ = [a for a in ans_candidate if a[int(k) - 1] == v]
         if len(ans_set) == 0:
-            ans_set = set([a for a in ans_candidate if c not in a])
+            ans_set = set(ans_)
         else:
-            ans_set = ans_set.intersection(set([a for a in ans_candidate if c not in a]) )
+            ans_set = ans_set.intersection(set(ans_) )
     return list(ans_set)
 
 #def count_alpha(ans_candidate):
     
-
+    
 #文字と場所による絞り込み
 #含まれている文字による絞り込み
 ans1 = character_position_select(ans0, pos_dict )
